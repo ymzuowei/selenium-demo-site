9e12819826855f075dda8c630e824e8a89116f49
Stanley Zuo <zuostanleywei@gmail.com>
2025-06-16 14:56:46 +0800

    fix github ci

--- tests/src/test/java/com/demo/base/BaseTest.java
+++ tests/src/test/java/com/demo/base/BaseTest.java
@@ -2,11 +2,18 @@ package com.demo.base;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebDriver;
 public class BaseTest {
   protected WebDriver driver;
   @BeforeEach public void setUp(){
-    driver = new ChromeDriver();
+    ChromeOptions options = new ChromeOptions();
+    // Run Chrome headless in CI
+    options.addArguments("--headless=new");  // or just "--headless" if your Chrome version doesn't support new headless mode yet
+    options.addArguments("--no-sandbox"); // Required in many CI environments
+    options.addArguments("--disable-dev-shm-usage"); // Overcome limited /dev/shm space in containers
+
+    driver = new ChromeDriver(options);
   }
   @AfterEach public void tearDown(){
     if(driver!=null) driver.quit();
